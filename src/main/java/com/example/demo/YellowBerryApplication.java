b4376cdd8a9acfa2ec4241d3a2eeb31f8760801a
Jérémy Grégoire <46073105+jeyofdev@users.noreply.github.com>
2025-02-15 10:36:14 +0100

    Config database (#1)
    * config : init application.properties
    
    * config : init database

---
+++ src/main/java/com/example/demo/YellowBerryApplication.java
@@ -0,0 +1,27 @@
+package com.example.demo;
+
+import com.example.demo.config.DatabaseConfig;
+import com.example.demo.util.DatabaseInitializer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class YellowBerryApplication {
+
+	public static void main(String[] args) {
+		ApplicationContext context = SpringApplication.run(YellowBerryApplication.class, args);
+		DatabaseConfig dbConfig = context.getBean(DatabaseConfig.class);
+
+		// Check if database exist
+		DatabaseInitializer.initializeDatabase(
+				"jdbc:postgresql://localhost:5432/postgres",
+				dbConfig.getDbUser(),
+				dbConfig.getDbPassword(),
+				dbConfig.getDbName()
+		);
+
+		System.out.println("hello world");
+	}
+
+}
