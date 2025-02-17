7c5d00c6bd5e2428f8eba9127f8b9c96c5341934
jeyofdev <jgregoire.dev@gmail.com>
2025-02-17 16:33:59 +0100

    refacto : add method sendEmail in EmailServiceImpl

--- src/main/java/com/jeyofdev/yellow_berry/auth/EmailServiceImpl.java
+++ src/main/java/com/jeyofdev/yellow_berry/auth/EmailServiceImpl.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.MailException;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
@@ -20,49 +21,52 @@ public class EmailServiceImpl implements EmailService {
     private String fromEmail;
 
     @Override
-    public void sendPasswordResetEmail(String toEmail, String resetToken) {
+    public void sendPasswordResetEmail(String email, String resetToken) {
         String resetUrl = MessageFormat.format("http://localhost:8080/api/v1/auth/reset-password?resetToken={0}", resetToken);
 
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setTo(toEmail);
-        message.setFrom(fromEmail);
-        message.setSubject("Reset your password");
-        message.setText(MessageFormat.format("To reset your password, please click on the following link : {0}", resetUrl));
-
-        emailSender.send(message);
+        sendEmail(
+                email,
+                "Reset your password",
+                MessageFormat.format("To reset your password, please click on the following link : {0}", resetUrl)
+        );
     }
 
     @Override
-    public void sendSuccessUpdatePasswordEmail(String toEmail) {
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setTo(toEmail);
-        message.setFrom(fromEmail);
-        message.setSubject("Password Updated");
-        message.setText("Your password has been successfully changed. You can now use your new password to log in to your account. If you didn’t make this change or if you have any issues, feel free to contact us as soon as possible. We’re here to help you keep your account safe.");
-
-        emailSender.send(message);
+    public void sendSuccessUpdatePasswordEmail(String email) {
+        sendEmail(
+                email,
+                "Password Updated",
+                "Your password has been successfully changed. You can now use your new password to log in to your account. If you didn’t make this change or if you have any issues, feel free to contact us as soon as possible. We’re here to help you keep your account safe."
+        );
     }
 
     @Override
     public void sendValidationEmail(String email, String verificationToken) {
         String verificationUrl = MessageFormat.format("http://localhost:8080/api/v1/auth/validate-account?verificationToken={0}", verificationToken);
 
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setTo(email);
-        message.setFrom(fromEmail);
-        message.setSubject("Verification of your account");
-        message.setText(MessageFormat.format("Please click the following link to verify your account: {0}", verificationUrl));
-
-        emailSender.send(message);
+        sendEmail(
+                email,
+                "Verification of your account",
+                MessageFormat.format("Please click the following link to verify your account: {0}", verificationUrl)
+        );
     }
 
     @Override
     public void sendSuccessValidationEmail(String email) {
+        sendEmail(
+                email,
+                "Confirmation of your account validation",
+                "We are pleased to inform you that your account has been successfully validated. You can now access all the features of our platform."
+        );
+    }
+
+    private void sendEmail(String toEmail, String subject, String body) throws MailException {
         SimpleMailMessage message = new SimpleMailMessage();
-        message.setTo(email);
-        message.setFrom(fromEmail);
-        message.setSubject("Confirmation of your account validation");
-        message.setText("We are pleased to inform you that your account has been successfully validated. You can now access all the features of our platform.");
+
+        message.setTo(toEmail);
+        message.setFrom(this.fromEmail);
+        message.setSubject(subject);
+        message.setText(body);
 
         emailSender.send(message);
     }
