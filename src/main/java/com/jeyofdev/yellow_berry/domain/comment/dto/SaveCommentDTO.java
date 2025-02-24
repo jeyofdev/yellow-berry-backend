7de4ef0541c17430dd5e1f9aeb69bf0160f3a56c
Jérémy Grégoire <46073105+jeyofdev@users.noreply.github.com>
2025-02-24 14:29:54 +0100

    feat : create model NameFormat and update mappers (#23)

--- src/main/java/com/jeyofdev/yellow_berry/domain/comment/dto/SaveCommentDTO.java
+++ src/main/java/com/jeyofdev/yellow_berry/domain/comment/dto/SaveCommentDTO.java
@@ -2,8 +2,6 @@ package com.jeyofdev.yellow_berry.domain.comment.dto;
 
 
 public record SaveCommentDTO(
-        String firstname,
-        String lastname,
         Integer rating,
         String body
 ) {
