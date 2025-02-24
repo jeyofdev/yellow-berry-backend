94063c703e5c5849cc2afafd4eab8d09ca6974c7
Jérémy Grégoire <46073105+jeyofdev@users.noreply.github.com>
2025-02-24 14:45:37 +0100

    feat : add list product in tagDTO and categoryDTO (#24)

--- src/main/java/com/jeyofdev/yellow_berry/domain/category/CategoryMapper.java
+++ src/main/java/com/jeyofdev/yellow_berry/domain/category/CategoryMapper.java
@@ -1,11 +1,15 @@
 package com.jeyofdev.yellow_berry.domain.category;
 
+import com.jeyofdev.yellow_berry.core.mappers.ListResponseFormatMapper;
 import com.jeyofdev.yellow_berry.domain.category.dto.CategoryDTO;
 import com.jeyofdev.yellow_berry.domain.category.dto.SaveCategoryDTO;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", uses = ListResponseFormatMapper.class)
 public interface CategoryMapper {
+    @Mapping(source = "productList", target = "products", qualifiedByName = "toListResponseFormat")
     CategoryDTO mapFromEntity(Category category);
+
     Category mapToEntity(SaveCategoryDTO saveCategoryDTO);
 }
