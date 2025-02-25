6965105c4aa5115a96ae386762fb5c507c77eb49
Jérémy Grégoire <46073105+jeyofdev@users.noreply.github.com>
2025-02-25 17:24:54 +0100

    Format team member dtos (#28)
    * feat : create socialFormat and use it in team-member dto
    
    * feat : format response name in team-member and testimonial

--- src/main/java/com/jeyofdev/yellow_berry/domain/team_member/TeamMemberMapper.java
+++ src/main/java/com/jeyofdev/yellow_berry/domain/team_member/TeamMemberMapper.java
@@ -3,9 +3,15 @@ package com.jeyofdev.yellow_berry.domain.team_member;
 import com.jeyofdev.yellow_berry.domain.team_member.dto.TeamMemberDTO;
 import com.jeyofdev.yellow_berry.domain.team_member.dto.SaveTeamMemberDTO;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface TeamMemberMapper {
+    @Mapping(source = "twitter", target = "social.twitter")
+    @Mapping(source = "instagram", target = "social.instagram")
+    @Mapping(source = "linkedin", target = "social.linkedin")
+    @Mapping(target = "name", expression = "java(teamMember.getFirstname() + \" \" + teamMember.getLastname())")
     TeamMemberDTO mapFromEntity(TeamMember teamMember);
+
     TeamMember mapToEntity(SaveTeamMemberDTO saveTeamMemberDTO);
 }
--- src/main/java/com/jeyofdev/yellow_berry/domain/team_member/dto/TeamMemberDTO.java
+++ src/main/java/com/jeyofdev/yellow_berry/domain/team_member/dto/TeamMemberDTO.java
@@ -1,16 +1,14 @@
 package com.jeyofdev.yellow_berry.domain.team_member.dto;
 
 import com.jeyofdev.yellow_berry.core.enums.JobEnum;
+import com.jeyofdev.yellow_berry.core.model.SocialFormat;
 
 import java.util.UUID;
 
 public record TeamMemberDTO(
         UUID id,
-        String firstname,
-        String lastname,
+        String name,
         JobEnum job,
-        String twitter,
-        String instagram,
-        String linkedin
+        SocialFormat social
 ) {
 }
