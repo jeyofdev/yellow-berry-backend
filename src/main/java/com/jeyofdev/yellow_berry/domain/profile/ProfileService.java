b502d49407786fc18ff0843042408dd6e58fff67
Jérémy Grégoire <46073105+jeyofdev@users.noreply.github.com>
2025-02-27 16:56:24 +0100

    Protected routes (#32)
    * config : set accessible routes with certain authentication roles
    
    * fix : condition the recovery of the exception message for ConstraintViolationException
    
    * feat : check that entities belong to the logged in user

--- src/main/java/com/jeyofdev/yellow_berry/domain/profile/ProfileService.java
+++ src/main/java/com/jeyofdev/yellow_berry/domain/profile/ProfileService.java
@@ -5,12 +5,17 @@ import com.jeyofdev.yellow_berry.auth_user.AuthUserService;
 import com.jeyofdev.yellow_berry.core.classes.AbstractDomainService;
 import com.jeyofdev.yellow_berry.core.constant.ConfirmMessage;
 import com.jeyofdev.yellow_berry.core.constant.ErrorMessage;
+import com.jeyofdev.yellow_berry.domain.product.Product;
+import com.jeyofdev.yellow_berry.domain.wishlist.WishList;
 import com.jeyofdev.yellow_berry.exception.AlreadyTakenException;
 import com.jeyofdev.yellow_berry.exception.AlreadyAssociatedException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 import java.text.MessageFormat;
+import java.util.List;
 import java.util.UUID;
 
 @Service
@@ -26,41 +31,57 @@ public class ProfileService extends AbstractDomainService<Profile, ProfileReposi
     }
 
     public Profile save(UUID userId, Profile profile) {
+        String username  = SecurityContextHolder.getContext().getAuthentication().getName();
         AuthUser user = authUserService.findUserById(userId);
 
-        if (user.getProfile() != null) {
-            throw new AlreadyAssociatedException(MessageFormat.format(ErrorMessage.ALREADY_ASSOCIATED, "user", "profile"));
+        if (username.equals(user.getUsername())) {
+            if (user.getProfile() != null) {
+                throw new AlreadyAssociatedException(MessageFormat.format(ErrorMessage.ALREADY_ASSOCIATED, "user", "profile"));
+            }
+
+            if (profileRepository.existsByPhone(profile.getPhone())) {
+                throw new AlreadyTakenException(MessageFormat.format(ErrorMessage.ALREADY_TAKEN, entityName, "phone", profile.getPhone()));
+            }
+
+            profile.setUser(user);
+
+            return profileRepository.save(profile);
+        } else {
+            throw new AccessDeniedException(ErrorMessage.LIMIT_ACCESS);
         }
-
-        if (profileRepository.existsByPhone(profile.getPhone())) {
-            throw new AlreadyTakenException(MessageFormat.format(ErrorMessage.ALREADY_TAKEN, entityName, "phone", profile.getPhone()));
-        }
-
-
-        profile.setUser(user);
-
-        return profileRepository.save(profile);
     }
 
     public Profile updateById(UUID profileId, Profile updatedProfile) {
+        String username  = SecurityContextHolder.getContext().getAuthentication().getName();
         Profile existingProfile = findById(profileId);
 
-        existingProfile.setFirstname(updatedProfile.getFirstname() != null ? updatedProfile.getFirstname() : existingProfile.getFirstname());
-        existingProfile.setLastname(updatedProfile.getLastname() != null ? updatedProfile.getLastname() : existingProfile.getLastname());
-        existingProfile.setPhone(updatedProfile.getPhone() != null ? updatedProfile.getPhone() : existingProfile.getPhone());
-        existingProfile.setAddress(updatedProfile.getAddress() != null ? updatedProfile.getAddress() : existingProfile.getAddress());
-        existingProfile.setRegion(updatedProfile.getRegion() != null ? updatedProfile.getRegion() : existingProfile.getRegion());
-        existingProfile.setDepartment(updatedProfile.getDepartment() != null ? updatedProfile.getDepartment() : existingProfile.getDepartment());
-        existingProfile.setZipCode(updatedProfile.getZipCode() != null ? updatedProfile.getZipCode() : existingProfile.getZipCode());
-        existingProfile.setCity(updatedProfile.getCity() != null ? updatedProfile.getCity() : existingProfile.getCity());
+        if (username.equals(existingProfile.getUser().getUsername())) {
+            existingProfile.setFirstname(updatedProfile.getFirstname() != null ? updatedProfile.getFirstname() : existingProfile.getFirstname());
+            existingProfile.setLastname(updatedProfile.getLastname() != null ? updatedProfile.getLastname() : existingProfile.getLastname());
+            existingProfile.setPhone(updatedProfile.getPhone() != null ? updatedProfile.getPhone() : existingProfile.getPhone());
+            existingProfile.setAddress(updatedProfile.getAddress() != null ? updatedProfile.getAddress() : existingProfile.getAddress());
+            existingProfile.setRegion(updatedProfile.getRegion() != null ? updatedProfile.getRegion() : existingProfile.getRegion());
+            existingProfile.setDepartment(updatedProfile.getDepartment() != null ? updatedProfile.getDepartment() : existingProfile.getDepartment());
+            existingProfile.setZipCode(updatedProfile.getZipCode() != null ? updatedProfile.getZipCode() : existingProfile.getZipCode());
+            existingProfile.setCity(updatedProfile.getCity() != null ? updatedProfile.getCity() : existingProfile.getCity());
 
-        return profileRepository.save(existingProfile);
+            return profileRepository.save(existingProfile);
+        } else {
+            throw new AccessDeniedException(ErrorMessage.LIMIT_ACCESS);
+        }
     }
 
     public String deleteById(UUID profileId) {
-        findById(profileId);
-        profileRepository.deleteById(profileId);
+        String username  = SecurityContextHolder.getContext().getAuthentication().getName();
+        String roles  = SecurityContextHolder.getContext().getAuthentication().getAuthorities().toString();
+        Profile profile = findById(profileId);
 
-        return ConfirmMessage.PROFILE_DELETE;
+        if (username.equals(profile.getUser().getUsername()) || roles.equals("[ROLE_ADMIN]")) {
+            profileRepository.deleteById(profileId);
+
+            return ConfirmMessage.PROFILE_DELETE;
+        } else {
+            throw new AccessDeniedException(ErrorMessage.LIMIT_ACCESS);
+        }
     }
 }
--- src/main/java/com/jeyofdev/yellow_berry/security/config/SecurityConfig.java
+++ src/main/java/com/jeyofdev/yellow_berry/security/config/SecurityConfig.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -39,11 +40,116 @@ public class SecurityConfig {
 
                 // Liste des routes protégées / non protégées
                 .authorizeHttpRequests((requests) -> requests
-                        .anyRequest().permitAll()
-                        /*.requestMatchers("/api/v1/auth/**").permitAll()
-                        .requestMatchers(HttpMethod.GET, "/api/v1/users/**").hasAnyRole(RoleEnum.ADMIN.name(), RoleEnum.USER.name())
-                        .requestMatchers(HttpMethod.POST, "/api/v1/auth/update-password").hasAnyRole(RoleEnum.ADMIN.name(), RoleEnum.USER.name())
-                        .anyRequest().authenticated()*/
+                        .requestMatchers("/api/v1/auth/**").permitAll()
+
+                        .requestMatchers(
+                                HttpMethod.GET,
+                                "/api/v1/about/**",
+                                "/api/v1/brand/**",
+                                "/api/v1/category/**",
+                                "/api/v1/faq/**",
+                                "/api/v1/product/**",
+                                "/api/v1/product_details/**",
+                                "/api/v1/product_information/**",
+                                "/api/v1/service/**",
+                                "/api/v1/tag/**",
+                                "/api/v1/team/member/**",
+                                "/api/v1/testimonial/**",
+                                "/api/v1/comment/{commentId}",
+                                "/api/v1/product/**"
+                        ).permitAll()
+
+                        .requestMatchers(HttpMethod.GET,
+                                "/api/v1/users",
+                                "/api/v1/wishlist",
+                                "/api/v1/profile",
+                                "/api/v1/cart",
+                                "/api/v1/product",
+                                "/api/v1/comment"
+                        ).hasRole(RoleEnum.ADMIN.name())
+
+                        .requestMatchers(HttpMethod.GET,
+                                "/api/v1/users/email/{email}",
+                                "/api/v1/users/{userId}",
+                                "/api/v1/wishlist/{wishlistId}",
+                                "/api/v1/profile/{profileId}",
+                                "/api/v1/cart/{cartId}"
+                        ).hasAnyRole(RoleEnum.ADMIN.name(), RoleEnum.USER.name())
+
+                        .requestMatchers(HttpMethod.POST,
+                                "/api/v1/auth/update-password",
+                                "/api/v1/comment/product/{productId}/profile/{profileId}"
+                        ).hasAnyRole(RoleEnum.ADMIN.name(), RoleEnum.USER.name())
+
+                        .requestMatchers(HttpMethod.POST,
+                                "/api/v1/about",
+                                "/api/v1/brand",
+                                "/api/v1/category",
+                                "/api/v1/faq",
+                                "/api/v1/product_details/product/{productId}",
+                                "/api/v1/product_information/product/{productId}",
+                                "/api/v1/service",
+                                "/api/v1/tag",
+                                "/api/v1/team/member",
+                                "/api/v1/testimonial",
+                                "/api/v1/product"
+                        ).hasRole(RoleEnum.ADMIN.name())
+
+                        .requestMatchers(HttpMethod.POST,
+                                "/api/v1/wishlist/profile/{profileId}",
+                                "/api/v1/profile/user{userId}",
+                                "/api/v1/cart/profile/{profileId}",
+                                "/api/v1/product/{productId}/add/cart/{cartId}",
+                                "/api/v1/product/{productId}/remove/cart/{cartId}",
+                                "/api/v1/product/{productId}/wishlist/{wishlistId}"
+                        ).hasRole(RoleEnum.USER.name())
+
+                        .requestMatchers(HttpMethod.PUT,
+                                "/api/v1/about/{aboutId}",
+                                "/api/v1/brand/{brandId}",
+                                "/api/v1/category/{categoryId}",
+                                "/api/v1/faq/{faqId}",
+                                "/api/v1/product_details/{faqId}",
+                                "/api/v1/product_information/{productInformationId}",
+                                "/api/v1/service/{serviceId}",
+                                "/api/v1/tag/{tagId}",
+                                "/api/v1/team/member/{teamMemberId}",
+                                "/api/v1/testimonial/{testimonialId}",
+                                "/api/v1/product/{productId}"
+                        ).hasRole(RoleEnum.ADMIN.name())
+
+                        .requestMatchers(HttpMethod.PUT,
+                                "/api/v1/wishlist/{wishlistId}",
+                                "/api/v1/profile/{profileId}",
+                                "/api/v1/cart/{cartId}"
+                        ).hasRole(RoleEnum.USER.name())
+
+                        .requestMatchers(HttpMethod.PUT,
+                                "/api/v1/comment/{commentId}"
+                        ).hasAnyRole(RoleEnum.ADMIN.name(), RoleEnum.USER.name())
+
+                        .requestMatchers(HttpMethod.DELETE,
+                                "/api/v1/brand/{brandId}",
+                                "/api/v1/category/{categoryId}",
+                                "/api/v1/faq/{faqId}",
+                                "/api/v1/product_details/**",
+                                "/api/v1/product_information/{productInformationId}",
+                                "/api/v1/service/{serviceId}",
+                                "/api/v1/tag/{tagId}",
+                                "/api/v1/team/member/{teamMemberId}",
+                                "/api/v1/testimonial/{testimonialId}",
+                                "/api/v1/product/{productId}",
+                                "/wishlist/{wishlistId}"
+                        ).hasRole(RoleEnum.ADMIN.name())
+
+                        .requestMatchers(HttpMethod.DELETE,
+                                "/api/v1/wishlist/{wishlistId}",
+                                "/api/v1/profile/{profileId}",
+                                "/api/v1/cart/{cartId}",
+                                "/api/v1/comment/{commentId}"
+                        ).hasAnyRole(RoleEnum.ADMIN.name(), RoleEnum.USER.name())
+
+                       .anyRequest().authenticated()
                 )
 
                 // authentication errors
