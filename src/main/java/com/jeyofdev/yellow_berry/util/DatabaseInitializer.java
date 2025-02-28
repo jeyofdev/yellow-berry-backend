7cc242d6f507b156048d31f4e498798c3c7a0a4f
jeyofdev <jgregoire.dev@gmail.com>
2025-02-28 14:54:38 +0100

    feat : create fake categories and tags from DatabaseInitializer

--- src/main/java/com/jeyofdev/yellow_berry/util/DatabaseInitializer.java
+++ src/main/java/com/jeyofdev/yellow_berry/util/DatabaseInitializer.java
@@ -3,6 +3,12 @@ package com.jeyofdev.yellow_berry.util;
 import com.github.javafaker.Faker;
 import com.jeyofdev.yellow_berry.domain.brand.*;
 import com.jeyofdev.yellow_berry.domain.brand.dto.SaveBrandDTO;
+import com.jeyofdev.yellow_berry.domain.category.CategoryController;
+import com.jeyofdev.yellow_berry.domain.category.CategoryRepository;
+import com.jeyofdev.yellow_berry.domain.category.dto.SaveCategoryDTO;
+import com.jeyofdev.yellow_berry.domain.tag.TagController;
+import com.jeyofdev.yellow_berry.domain.tag.TagRepository;
+import com.jeyofdev.yellow_berry.domain.tag.dto.SaveTagDTO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -14,9 +20,14 @@ import java.util.stream.IntStream;
 @RequiredArgsConstructor
 public class DatabaseInitializer implements CommandLineRunner {
     private final BrandRepository brandRepository;
-    private final BrandMapper brandMapper;
-    private final BrandService brandService;
     private final BrandController brandController;
+
+    private final CategoryRepository categoryRepository;
+    private final CategoryController categoryController;
+
+    private final TagRepository tagRepository;
+    private final TagController tagController;
+
     private final Faker faker = new Faker();
 
     /*public static void initializeDatabase(String jdbcUrl, String user, String password, String dbName) {
@@ -44,14 +55,49 @@ public class DatabaseInitializer implements CommandLineRunner {
 
     private void createDatas() throws IOException {
         System.out.println("Database initialization started...");
-        this.createBrand();
+        this.createFakeBrands();
+        this.createFakeCategories();
+        this.createFakeTags();
     }
 
-    private void createBrand() {
+    private void createFakeBrands() {
         if (brandRepository.count() == 0) {
             IntStream.range(0, 10).forEach(i -> {
-                SaveBrandDTO saveBrandDTO = new SaveBrandDTO(faker.company().name());
+                String brandName;
+                do {
+                    brandName = faker.company().name();
+                } while (brandRepository.existsByName(brandName));
+
+                SaveBrandDTO saveBrandDTO = new SaveBrandDTO(brandName);
                 brandController.saveBrand(saveBrandDTO);
+            });
+        }
+    }
+
+    private void createFakeCategories() {
+        if (categoryRepository.count() == 0) {
+            IntStream.range(0, 10).forEach(i -> {
+                String categoryName;
+                do {
+                    categoryName = faker.commerce().department();
+                } while (categoryRepository.existsByName(categoryName));
+
+                SaveCategoryDTO saveCategoryDTO = new SaveCategoryDTO(categoryName);
+                categoryController.saveCategory(saveCategoryDTO);
+            });
+        }
+    }
+
+    private void createFakeTags() {
+        if (tagRepository.count() == 0) {
+            IntStream.range(0, 10).forEach(i -> {
+                String tagName;
+                do {
+                    tagName = faker.food().fruit();
+                } while (tagRepository.existsByName(tagName));
+
+                SaveTagDTO saveTagDTO = new SaveTagDTO(tagName);
+                tagController.saveTag(saveTagDTO);
             });
         }
     }
