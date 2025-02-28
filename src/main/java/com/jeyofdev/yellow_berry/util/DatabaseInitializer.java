c7c35158d3b603fcb0fa8525037f5d23a91b9e65
jeyofdev <jgregoire.dev@gmail.com>
2025-02-28 15:14:55 +0100

    feat : create fake product from initializer

--- src/main/java/com/jeyofdev/yellow_berry/util/DatabaseInitializer.java
+++ src/main/java/com/jeyofdev/yellow_berry/util/DatabaseInitializer.java
@@ -1,19 +1,29 @@
 package com.jeyofdev.yellow_berry.util;
 
 import com.github.javafaker.Faker;
+import com.jeyofdev.yellow_berry.core.enums.StockEnum;
+import com.jeyofdev.yellow_berry.core.enums.WeightEnum;
 import com.jeyofdev.yellow_berry.domain.brand.*;
 import com.jeyofdev.yellow_berry.domain.brand.dto.SaveBrandDTO;
+import com.jeyofdev.yellow_berry.domain.category.Category;
 import com.jeyofdev.yellow_berry.domain.category.CategoryController;
 import com.jeyofdev.yellow_berry.domain.category.CategoryRepository;
 import com.jeyofdev.yellow_berry.domain.category.dto.SaveCategoryDTO;
+import com.jeyofdev.yellow_berry.domain.product.ProductController;
+import com.jeyofdev.yellow_berry.domain.product.ProductRepository;
+import com.jeyofdev.yellow_berry.domain.product.dto.SaveProductDTO;
 import com.jeyofdev.yellow_berry.domain.tag.TagController;
 import com.jeyofdev.yellow_berry.domain.tag.TagRepository;
 import com.jeyofdev.yellow_berry.domain.tag.dto.SaveTagDTO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.io.IOException;
+import java.util.List;
+import java.util.Random;
+import java.util.UUID;
 import java.util.stream.IntStream;
 
 @Component
@@ -28,7 +38,11 @@ public class DatabaseInitializer implements CommandLineRunner {
     private final TagRepository tagRepository;
     private final TagController tagController;
 
+    private final ProductRepository productRepository;
+    private final ProductController productController;
+
     private final Faker faker = new Faker();
+    private final Random random = new Random();
 
     /*public static void initializeDatabase(String jdbcUrl, String user, String password, String dbName) {
         String createDbQuery = MessageFormat.format("CREATE DATABASE {0}", dbName);
@@ -58,6 +72,7 @@ public class DatabaseInitializer implements CommandLineRunner {
         this.createFakeBrands();
         this.createFakeCategories();
         this.createFakeTags();
+        this.createFakeProducts();
     }
 
     private void createFakeBrands() {
@@ -100,5 +115,45 @@ public class DatabaseInitializer implements CommandLineRunner {
                 tagController.saveTag(saveTagDTO);
             });
         }
+    }
+
+    @Transactional
+    private void createFakeProducts() {
+        List<Brand> brands = brandRepository.findAll();
+        List<Category> categories = categoryRepository.findAll();
+
+        IntStream.range(0, 10).forEach(i -> {
+            String productName;
+            do {
+                productName = faker.commerce().productName();
+            } while (productRepository.existsByName(productName));
+
+            int rating = faker.options().option(1, 2, 3, 4, 5);
+            double price = faker.number().randomDouble(2, 10, 500);
+            double discount = faker.number().randomDouble(2, 0, 50);
+            double priceDiscount = price - (price * discount / 100);
+            StockEnum stock = StockEnum.IN_STOCK;
+            WeightEnum weight = WeightEnum.GRAM_250;
+            UUID brandId = brands.get(random.nextInt(brands.size())).getId();
+            UUID categoryId = categories.get(random.nextInt(categories.size())).getId();
+
+            SaveProductDTO saveProductDTO = new SaveProductDTO(
+                    productName,
+                    rating,
+                    price,
+                    priceDiscount,
+                    discount,
+                    stock,
+                    weight,
+                    List.of(), // Tags
+                    List.of(categoryId),
+                    List.of(), // Comments
+                    List.of(), // Wishlist
+                    List.of(), // Cart
+                    brandId
+            );
+
+            productController.saveProduct(saveProductDTO);
+        });
     }
 }
