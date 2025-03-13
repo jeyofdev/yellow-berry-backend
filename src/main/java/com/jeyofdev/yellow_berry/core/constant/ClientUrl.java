4e2f6e9feeb3b0d465466cecc51ed5bfd2f68bad
Jérémy Grégoire <46073105+jeyofdev@users.noreply.github.com>
2025-03-13 19:35:33 +0100

    feat : add constants client-url (#42)

--- src/main/java/com/jeyofdev/yellow_berry/core/constant/ClientUrl.java
+++ src/main/java/com/jeyofdev/yellow_berry/core/constant/ClientUrl.java
@@ -7,32 +7,19 @@ import org.springframework.stereotype.Component;
 
 @Component
 @Getter
-public class Url {
+public class ClientUrl {
 
-    @Value("${app.base-api-url}")
+    @Value("${app.base-client-url}")
     private String baseUrl;
 
-    @Value("${app.base-api-version}")
-    private String apiVersion;
-
     private static String BASE_URL;
-    private static String API_VERSION;
 
     @PostConstruct
     private void init() {
         BASE_URL = baseUrl;
-        API_VERSION = apiVersion;
     }
 
     public static String getBaseUrl() {
         return BASE_URL;
-    }
-
-    public static String getApiVersion() {
-        return BASE_URL;
-    }
-
-    public static String getFullBaseUrl() {
-        return BASE_URL + "/" + API_VERSION;
     }
 }
